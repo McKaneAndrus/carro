cac5337fae8dcea9a290e8f9dde576d37eab1b8f
Sandy <sandy.revmaker@gmail.com>
2014-03-11 18:29:08 -0700

    Initial commit of new language file. This is a site wide file so far

---
+++ protected/messages/pt_br/LeadGen.php
@@ -0,0 +1,84 @@
+<?php
+// pt_br langange translations for landing.php
+// format is 'English' => 'Portuguese'
+
+
+return array(
+
+	// view translates
+	
+	'Select a Make' =>'pt_br Select a Make',
+	'Select a Model' => 'pt_br Select a Model',
+	'Select a Trim' => 'pt_br Select a Trim', 
+	'Select a Color' => 'pt_br Select a Color',
+	'Any Trim' => 'pt_br Any Trim',
+	'Any Color' => 'pt_br Any Color',
+	'Unknown City' => 'pt_br Unknown City',
+	'Unknown State' => 'pt_br Unknown State',
+	'Unknown Make' => 'pt_br Unknown Make',
+	'Unknown Model' => 'pt_br Unknown Model',
+	'Unknown Trim' => 'pt_br Unknown Trim',
+	'Unknown Color' => 'pt_br Unknown Color',
+	
+	// landing page 
+	
+	
+	'Let us help you get the best deal' => 'Vamos ajudá-lo a obter o melhor negócio',
+	'Before you buy a new car, compare prices and financing from several dealers and banks' => 'Antes de comprar um carro novo, comparar preços e financiamento de vários comerciantes e bancos',
+	'Get Started' => 'começar',
+	
+	// quote page
+	
+	'1. Select Trim and Color' => '1. selecionar aparar e cor',
+	'2. Select Dealers' => '2. Selecionar Dealers',
+	'3. Enter Your Info' => '3. Incorpore sua informação',
+	'Trim' => 'Aparar',
+	'Color' => 'Cor',
+	
+	'Unable to Locate Local Dealers' => 'Impossível Localizar Revendedor',
+	'Best Dealers in Your Area' => 'Concessionários em sua área',
+	'More Dealers' => 'Mais Dealers',
+	'Carro Specialist will forward request,' => 'Carro especialista encaminhará pedido.',
+	'Please continue with submission' => 'Por favor, continuem com a submissão',
+	'Thank You' => 'Obrigado',
+	'Get Your Price Now' => 'Receba seu preço agora',
+
+	// Confirm page 
+	
+	'Would you like to get another quote?' => 'Gostaria de ter outra citação?',
+	'Get Another Quote' => 'Obter outra citação',
+	'Thank you for your request' =>'Obrigado por seu pedido',
+	'Your selected vehicle' => 'Seu veículo selecionado',
+	'Save even more with these offers' => 'Economize ainda mais com estas ofertas',
+	'Affiliate' => 'afiliar',
+	'Get Deal' => 'Aproveite a Oferta',
+	
+	
+	// field names
+	
+	'First Name' => 'Nome',
+	'Last Name' => 'Sobrenome',
+	'Telephone' => 'Telefone',
+	'Email' => 'Email',
+	'Street Address' => 'Endereço',
+	'Message' => 'Mensagem',
+	'Make'=> 'Fabricante',
+	'Model'=>'Modelo',
+	'Zip Code' => 'Código Postal',	// might be better in a leadgen context...
+	
+	// field error messages
+	
+	'Please Select a Make'=>'Selecione um fabricante',
+	'Please Select a Model' =>'Selecione um Modelo',
+	'Please Enter a Postal Code' => 'Introduza um código postal',
+	'Invalid Format, use 00000-000' => 'Formato inválido, usar 00000-000',
+	'Please Select a Trim'=>'Selecione uma guarnição',
+	'Please Select a Color' => 'Selecione uma cor',
+	'Last Name Required' => 'Apelido Obrigatório',
+	'First Name Required' => 'Nome Obrigatório',
+	'Email Required' => 'Email Obrigatório',
+	'Invalid Email Address' => 'Endereço inválido Email',
+	'Invalid Phone Number' => 'Número de telefone inválido',
+	
+);
+?>
