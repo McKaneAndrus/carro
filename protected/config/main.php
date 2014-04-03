d0f50ecfd80428d41fa6a2fa19e3453f927cb7dd
Sandy <sandy.revmaker@gmail.com>
2014-04-02 20:14:30 -0700

    commented out Gii access for production

--- protected/config/main.php
+++ protected/config/main.php
@@ -29,13 +29,14 @@ return array(
 		// uncomment the following to enable the Gii tool
 		// sjg - remove for production
 		//
-		
+	/*	
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=> false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
+		*/
 	),
 
 	// application components
