6b534e6f2ee9ca7dd475418ff065d96561797be9
Sandy <sandy.revmaker@gmail.com>
2014-04-25 18:41:01 -0700

    Removed Footer related to Carro

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -48,48 +48,14 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 
 	<div class="clear"></div>
 
-	<div id="footer"> <!-- sjg Added Carro Footer -->
+	<div id="footer"> 
         <footer>
             <div class="footer_wrapper">
                 <div>
                     <span class="footer_carro">
-                        <a href="/"><img width="150" height="46" src="http://carroonline.terra.com.br/sitestatic/images/logo-carro.png"></a>
-                        <a target="_blank" href="http://www.assinemotorpress.com.br/Assine-Carro.asp?Site_txt=ASSINEMOTORPRESS&amp;Origem_txt=&amp;Formato_txt=LINKDIRETO&amp;Banner_txt=&amp;Versao_txt=#.UfgjZZJQHzx"><img width="140" height="31" src="http://carroonline.terra.com.br/sitestatic/images/btn-assinar.png"></a>
                     </span>
-                    <a href="/" class="footer_motorpress"><img width="155" height="44" src="http://carroonline.terra.com.br/sitestatic/images/logo-motorpress.png"></a>
-                    <p>Copyright &copy; 2014<br />Editora MotorPress Brasil</p>
+                    <p>Copyright &copy; 2014<br />Revmaker</p>
                 </div>
-                
-                <div>
-                    <h5>Motorpress</h5>
-                    <ul>
-    					<li><a target="_blank" href="http://www.motorpressbrasil.com.br/anuncie.asp">Anuncie</a></li>
-    					<li><a rel="nofollow" target="_blank" href="http://www.assinemotorpress.com.br/">Assinaturas</a></li>
-    					<li><a target="_blank" href="http://www.assinemotorpress.com.br/faleconosco">Expediente</a></li>
-    					<li><a target="_self" href="http://carroonline.terra.com.br/contato">Fale Conosco</a></li>
-    				</ul>
-                </div>
-                
-                <div>
-                    <h5>Revistas</h5>
-                    <ul>
-					    <li><a target="_blank" href="http://motociclismo.terra.com.br/">Motociclismo</a></li>
-                        <li><a target="_blank" href="http://racing.terra.com.br/">Racing</a></li>
-                        <li><a target="_blank" href="http://sportlife.terra.com.br/">Sport Life</a></li>
-                        <li><a target="_blank" href="http://www.revistatransportemundial.com.br/">Transporte Mundial</a></li>
-                    </ul>
-				</div>
-				
-				<div>
-				    <h5>Na Rede</h5>
-                    <ul class="footer_social">
-					    <li><a target="_blank" href="http://carroonline.terra.com.br/rss"><span class="footer_rss"></span>RSS</a></li>
-                        <li><a target="_blank" href="http://www.facebook.com/revistacarro"><span class="footer_facebook"></span>Facebook</a></li>
-                        <li><a target="_blank" href="http://twitter.com/carroonline/"><span class="footer_twitter"></span>Twitter</a></li>
-                        <li><a target="_blank" href="http://www.youtube.com/carroonline "><span class="footer_youtube"></span>Youtube</a></li>
-					    <li><a target="_blank" href="http://instagram.com/revistacarro/"><span class="footer_instagram"></span>Instagram</a></li>
-                    </ul>
-				</div>
             </div>
         </footer>
 	</div><!-- footer -->
