ed8443be1c25e4dc1464a5d2df5642de1d9744ce
Sandy <sandy.revmaker@gmail.com>
2014-05-04 22:20:07 -0700

    Updated to include email globals, which include the sent by and send by name

--- protected/config/main.php
+++ protected/config/main.php
@@ -21,6 +21,8 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+		'ext.YiiMailer.YiiMailer',	// simple mailer component
+
 	),
 
 	'modules'=>array(
@@ -116,6 +118,8 @@ return array(
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@carro.br.com',
+		'AckEmailAdr' => 'suporte@achacarro.com',	// Thank You Email FROM address
+		'AckEmailName' => 'Suporte ao Cliente',				// Thank You Email Human Name
 		'version'=>'1.02',
 	),
 );
