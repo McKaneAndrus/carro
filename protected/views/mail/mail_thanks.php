edf00d647e5cfca6b9838d0620ff1b6b37088f14
Sandy <sandy.revmaker@gmail.com>
2014-05-04 22:16:24 -0700

    Thank you confirmation email, supports dealers and no dealer options internally

---
+++ protected/views/mail/mail_thanks.php
@@ -0,0 +1,58 @@
+<?php if(isset($name)) echo Yii::t('mail', 'Dear') . ' ' . $name . ',';?>
+<?php 
+
+	if(isset($dlr_list) && count($dlr_list) > 0)
+	{
+		if(isset($message)) 
+			echo '<p>' . $message . '</p>';
+
+		echo '<p>'; 
+		echo Yii::t('mail', 'We received your request for a price quote on a') . ' ';  
+		if(isset($make_name)) echo $make_name . ' '; 
+		if(isset($model_name)) echo $model_name . ' '; 
+		if(isset($year)) echo $year . ' '; 
+		if(isset($color)) echo $color; 
+		echo ' ' . Yii::t('mail','from') . '</p>'; 
+
+		Yii::t('mail', 'Below is a list of your selected dealers');
+		echo '<ul>';
+		foreach($dlr_list as $dlr_id)
+		{
+			$rec = $this->GetDealerInfo($dlr_id);
+
+			if(!empty($rec))
+			{
+				echo '<li>';
+				echo '<span style="color:#EC1D25;font-size:18px;">';
+				echo $rec['name'];
+				echo '</span>';
+				echo '<br>' . $rec['street'];
+				echo '<br>' . $rec['city'] . ', ' . $rec['state'];
+				echo '<br>' . $rec['postal'];
+				echo '<br>' . $rec['phone'];
+				echo '</li>';
+			}
+		}
+		echo '</ul>';
+
+		echo '<br>Eduardo Gonzales';
+	}
+	else
+	{	
+		// no dealers in the list
+		echo '<p>'; 
+		echo Yii::t('mail', 'We received your request for a price quote on a') . ' ';  
+		if(isset($make_name)) echo $make_name . ' '; 
+		if(isset($model_name)) echo $model_name . ' '; 
+		if(isset($year)) echo $year . ' '; 
+		if(isset($color)) echo $color; 
+		echo '. ' . Yii::t('mail', 'Unfortunately, we could not find a participating dealer near you.');
+		echo '<p>' . Yii::t('mail', 'Our apologies for the inconvenience,') . '</p>';
+		echo '<br>Lolita Gonzales';
+	}
+?>
+<?php Yii::t('mail', 'Customer Support') ?>
+<br><a href="http://www.achacarro.com/">Achacarro.com</a>
+<br>support@achacarro.com
+<p>	<?php if(isset($footer)) echo $footer; ?></p>
+
