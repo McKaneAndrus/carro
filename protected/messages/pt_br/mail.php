2f1d39e65150805ba693fd6f39302b0732f3bf4f
Sandy <sandy.revmaker@gmail.com>
2014-05-04 22:15:17 -0700

    tanslation for email messages

---
+++ protected/messages/pt_br/mail.php
@@ -0,0 +1,25 @@
+<?php
+// pt_br langange translations for thank you mail
+// format is 'English' => 'Portuguese'
+// Remeber strings must be exact to match a translation!
+// v1.0.0 Mail translations 
+// begat : 05/02/2014
+//
+
+return array(
+
+	'Achacarro Confirmation Email' => 'Achacarro Confirmação de E-mail',
+	'Customer Support' => 'Suporte ao Cliente',
+	'Your dealer will contact you by phone or email in the next 24 hours. Please email us if you don’t hear from them or need further assistance.' =>'Uma das nossas concessionárias entrará em contato com você por telefone ou email nas próximas 24 horas. Por favor, nos envie um email caso você não seja contatado ou tenha alguma dúvida.',
+
+	'Dear' => 'Prezado',
+	'from' => 'da',
+	
+	'We received your request for a price quote on a' => 'Nós recebemos seu pedido para cotação de um',
+	'Below is a list of your selected dealers' => 'Abaixo está uma lista de seus revendedores selecionados',
+	'Unfortunately, we could not find a participating dealer near you.' => 'Nesse momento não conseuimos localizar uma concessionária participante próxima.',
+	'Our apologies for the inconvenience' => 'Pedimos desculpas pelo inconveniente',
+	'We appreciate your business' => 'Nós apreciamos o seu negócio,',
+);
+?>
+
