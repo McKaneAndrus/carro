5b4f95d56f213edec3c51bcf97d0159d18fc24af
Sandy <sandy.revmaker@gmail.com>
2014-05-06 20:26:46 -0700

    Removed unused html and goggle analytics for now.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -35,29 +35,6 @@
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
-
-<!--
-	<div id="footer"> 
-        <footer>
-            <div class="footer_wrapper">
-                <div>
-                    <span class="footer_carro">
-                    </span>
-                    <p>Copyright &copy; 2014<br />Revmaker</p>
-                </div>
-            </div>
-        </footer>
-	</div>
--->
 </div><!-- page -->
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-42494165-1', 'carroglobo.com');
-  ga('send', 'pageview');
-</script>
 </body>
 </html>
