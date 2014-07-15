4b8bd8e7c028d0253ae9fa16bd929026cc77402a
Sandy <sandy.revmaker@gmail.com>
2014-07-14 22:20:57 -0700

    Added back in simple footer and cleaned some comments

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -12,7 +12,6 @@
 	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico" type="image/ico" />
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 	
-<!--	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/reset.css" /> -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" /> 
 
@@ -27,14 +26,25 @@
 <div class="container" id="page">
 	<div id="header">
             <div class="header_wrapper">
-                <a href="http://www.achacarro.com"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/achacarro_logo.png" alt="Achacarro" class="header_logo"/></a>
+                <a href="<?php echo Yii::app()->request->baseUrl; ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/achacarro_logo.png" alt="Achacarro" class="header_logo"/></a>
             </div>
 	</div><!-- header -->
 
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
+        <footer>
+            <div class="footer_wrapper">
+                <div>
+                    <span class="footer_carro">
+						<a href="<?php echo Yii::app()->request->baseUrl; ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/achacarro_logo_sm.png" alt="Achacarro" class="footer_logo"/></a>
+						<a href="<?php echo Yii::app()->request->baseUrl; ?>">Home</a> <a href="<?php echo Yii::app()->request->baseUrl; ?>/about">About</a> <a href="<?php echo Yii::app()->request->baseUrl; ?>/privacy">Privacy</a> Copyright &copy; 2014 Revmaker
+                    </span>
+                </div>
+            </div> <!-- footer_wrapper -->
+        </footer>
 </div><!-- page -->
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
