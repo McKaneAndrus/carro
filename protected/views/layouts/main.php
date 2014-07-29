ed32ada984f0caee6c0e05b4d35d8a2fac8e5f18
Sandy <sandy.revmaker@gmail.com>
2014-07-28 19:53:43 -0700

    Whitespace cleanup

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,16 +4,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="carro novo, cotacao gratis, preco do carro novo, Achacarro" />
-  	<meta name="description" content="Procurando uma otima oferta no seu carro novo? Nossa rede de concessionárias está interessada em lhe oferecer ótimas ofertas na compra do seu carro novo." />
-	<meta http-equiv="Content-Language" content="pt-BR" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="carro novo, cotacao gratis, preco do carro novo, Achacarro" />
+<meta name="description" content="Procurando uma otima oferta no seu carro novo? Nossa rede de concessionárias está interessada em lhe oferecer ótimas ofertas na compra do seu carro novo." />
+<meta http-equiv="Content-Language" content="pt-BR" />
 
-	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico" type="image/ico" />
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico" type="image/ico" />
+<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 	
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" /> 
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" /> 
 
 
 	<!--[if lt IE 8]>
