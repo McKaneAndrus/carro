4b696d918af1408d318cb36ec3c3ef28b39c9c91
Sandy <sandy.revmaker@gmail.com>
2014-08-11 20:41:33 -0700

    Version number update

--- protected/config/main.php
+++ protected/config/main.php
@@ -144,6 +144,6 @@ return array(
 		'AckEmailAdr' => 'suporte@achacarro.com', 	// Thank You Email FROM address
 		'AckEmailName' => 'Suporte ao Cliente',		// Thank You Email Human Name
 		'EmailDupeDays' => 7,						// Number of days to check back for dupes, 0 disables
-		'version'=>'2.20',
+		'version'=>'2.30',
 	),
 );
