dfe93c39c7f828ecfac65f2a982a85affb624929
McKane Andrus <mckaneandrus@gmail.com>
2014-08-19 15:07:48 -0700

    Adds quote_mobile and changes

---
+++ protected/views/layouts/landing_mobile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Acha Mobile</title>
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<!-- <script src="http://localhost/carro/protected/extensions/jquery.mobile-1.4.3/jquery.mobile-1.4.3.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://localhost/carro/protected/extensions/jquery.mobile-1.4.3/jquery.mobile-1.4.3.min.css"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.css">
+	<script src="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js"></script>
+
+    <style type="text/css">
+    	.wrapper {
+    	width: 500px;}
+
+    	.quote_mobile {
+    		background: url('/carro/images/red_shirt.jpg') no-repeat fixed;
+    		background-position: -50px;
+
+    	}
+
+    
+    
+    
+   </style>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="quote_mobile">
+			<img src="/carro/images/achacarro_logo.png" id="logo">
+		</div>
+	</div>
+</body>
+</html>
