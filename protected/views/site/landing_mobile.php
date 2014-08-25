deba842ca5001bd8d04dcc2686fe4e8ec7540123
McKane Andrus <mckaneandrus@gmail.com>
2014-08-25 13:49:47 -0700

    Adds mobile landing changes

--- protected/views/site/landing_mobile.php
+++ protected/views/site/landing_mobile.php
@@ -4,16 +4,18 @@
    <title>Acha Mobile</title>
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<!-- <script src="http://localhost/carro/protected/extensions/jquery.mobile-1.4.3/jquery.mobile-1.4.3.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://localhost/carro/protected/extensions/jquery.mobile-1.4.3/jquery.mobile-1.4.3.min.css"> -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.css">
 	<script src="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js"></script>
 
 	<script>
 	var heightDevice=$(window).height()
-	$(".wrapper").css("height", heightDevice);
+	$(".container-fluid").css("height", heightDevice);
 	</script>
 
     <style type="text/css">
@@ -27,11 +29,6 @@
     	bottom: 0;
     }
 
-    .wrapper {
-    	width: 100%;
-    	height: 100%;
-    }
-
     .quote_mobile {
     	background: url('/carro/images/red_shirt.jpg') no-repeat fixed;
     	background-position: 49px;
@@ -39,11 +36,8 @@
     	width: 100%;
     	height: 100% }
 
-    #logo {
-    	margin: 0 auto; }
-
-    @media screen and (max-width: 361px) {
-    	.wrapper {
+ /*   @media screen and (max-width: 361px) {
+    	.container-fluid {
     		width: 100%; }
 
     	.quote_mobile {
@@ -55,7 +49,7 @@
     	#logo {
     		width: 100%;
     	}
-	}
+	}*/
 
 
 
@@ -65,9 +59,11 @@
    </style>
 </head>
 <body>
-	<div class="wrapper">
-		<div class="quote_mobile">
-			<img src="/carro/images/achacarro_logo.png" id="logo">
+	<div class="quote_mobile">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12"><img src="/carro/images/achacarro_logo.png" id="logo"></div>
+			</div>
 		</div>
 	</div>
 </body>
