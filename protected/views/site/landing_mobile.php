08b33cff2d3e0ffd0d8c61a476351f31100bbb9c
McKane Andrus <mckaneandrus@gmail.com>
2014-08-26 12:56:47 -0700

    Adds mobile landing changes

--- protected/views/site/landing_mobile.php
+++ protected/views/site/landing_mobile.php
@@ -199,16 +199,16 @@
 				</div>
 			</div>
 		</div>
-		<!--<div class="row" style="position: absolute; bottom: 0px;">
+		<div class="row" style="position: absolute; bottom: 0px;">
 			<div class="col-xs-12">
-				<div id="footerLine"></div>
+				<div id="footerLine1"></div>
 				<ul class="img-responsive" id="footerNav">
 					<li><img src="/carro/images/achacarro_logo_xs.png"></li>
 				    <li><a href="http://achacarro.com/carro/about">Quem Somos</a></li>
 				    <li><a href="http://achacarro.com/carro/privacy">Politica de Privacidade</a></li>
 				    <li>Copyright © 2014 Revmaker</li>
 				</ul>
-			</div>-->
+			</div>
 		</div>
 	</div>
 
